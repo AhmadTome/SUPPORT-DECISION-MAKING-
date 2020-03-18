a85648b538b0c347420f00468ec46525885d11cf
AhmadTome <atome@asaltech.com>
2020-03-19 00:35:46 +0200

    prepate the statistic of lecturers

---
+++ supervisor/LecturerInfo.php
@@ -0,0 +1,226 @@
+<?php
+session_start();
+?>
+
+<!doctype html>
+<html class="fixed">
+<head>
+    <title>معلومات المحاضرين</title>
+    <!-- Basic -->
+    <meta charset="UTF-8">
+
+    <meta name="keywords" content="HTML5 Admin Template"/>
+    <meta name="description" content="Porto Admin - Responsive HTML5 Template">
+    <meta name="author" content="okler.net">
+
+    <!-- Vendor CSS -->
+    <link rel="stylesheet" href="../assets/vendor/bootstrap/css/bootstrap.css" />
+    <link rel="stylesheet" href="../assets/vendor/font-awesome/css/font-awesome.css" />
+    <link rel="stylesheet" href="../assets/vendor/magnific-popup/magnific-popup.css" />
+    <link rel="stylesheet" href="../assets/vendor/bootstrap-datepicker/css/datepicker3.css" />
+
+    <!-- Theme CSS -->
+    <link rel="stylesheet" href="../assets/stylesheets/theme.css" />
+
+    <!-- Skin CSS -->
+    <link rel="stylesheet" href="../assets/stylesheets/skins/default.css" />
+
+    <!-- Theme Custom CSS -->
+    <link rel="stylesheet" href="../assets/stylesheets/theme-custom.css">
+
+    <!--===============================================================================================-->
+    <link rel="icon" type="image/png" href="../assets/images/icons/favicon.ico"/>
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="../assets/vendor/bootstrap/css/bootstrap.min.css">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="../assets/fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="../assets/fonts/Linearicons-Free-v1.0.0/icon-font.min.css">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="../assets/vendor/animate/animate.css">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="../assets/vendor/css-hamburgers/hamburgers.min.css">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="../assets/vendor/animsition/css/animsition.min.css">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="../assets/vendor/select2/select2.min.css">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="../assets/vendor/daterangepicker/daterangepicker.css">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="../assets/css/util.css">
+    <link rel="stylesheet" type="text/css" href="../assets/css/main.css">
+
+<style>
+    th{
+        text-align: right !important;
+    }
+</style>
+</head>
+<body>
+<?php
+include('navbar.html')
+?>
+<!-- start: page -->
+<div class="limiter">
+    <div class="container-login100">
+        <div class="wrap-login100">
+            <div style="width: 100%;background-image: url(../img/logo.png);height: 150px; background-repeat: no-repeat;background-position: center;">
+
+            </div>
+
+            <?php
+
+            $info = getInfo();
+            ?>
+            <p class="text-left" style="color: red">
+                <?php
+                if (isset($_SESSION['Error'])) {
+                    echo $_SESSION['Error'];
+
+                    unset($_SESSION['Error']);
+
+                }
+                ?>
+            </p>
+            <p class="text-left" style="color: white; background-color: green" >
+                <?php
+                if( isset($_SESSION['success']) )
+                {
+                    echo $_SESSION['success'];
+
+                    unset($_SESSION['success']);
+
+                }
+                ?>
+            </p>
+            <form action="../database/editcatinfo.php" method="post" style="padding: 50px;">
+                <div class="form-group mb-lg">
+                    <label class="pull-right" style="font-size: 24px;">معلومات المحاضرين</label>
+                    <div class="input-group input-group-icon">
+                        <table class="table text-right"  dir="rtl">
+                            <thead >
+                            <tr >
+                                <th>الاسم</th>
+                                <th>الرقم</th>
+                                <th > الايميل</th>
+                                <th > السيرة الذاتية</th>
+                            </tr>
+                            </thead>
+                            <br>
+
+                            <?php
+
+                                for ($i=0;$i<count($info);$i++){
+                                    echo ' <tr>
+                                                <td>'. $info[$i]["name"] .'</td>
+                                                <td>'. $info[$i]["number"] .'</td>
+                                                <td>'. $info[$i]["email"] .'</td>
+                                                <td>الاستاذ : '. $info[$i]["name"] .'<br/> من مواليد '. $info[$i]["age"] .'<br/>   الجنس '. $info[$i]["gender"] .'  <br/>  الدرجة العلمية '. $info[$i]["degree"] .'
+                                                    <br/>رقم الهاتف '. $info[$i]["mobile_no"] .'  <br/>  من دولة '. $info[$i]["country"] .'
+                                                </td>
+                                           </tr>';
+                                }
+
+                            ?>
+
+                            <br/>
+
+
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </form>
+
+
+
+
+
+        </div>
+    </div>
+</div>
+<!-- end: page -->
+<!-- Vendor -->
+<script src="../assets/vendor/jquery/jquery.js"></script>
+<script src="../assets/vendor/jquery-browser-mobile/jquery.browser.mobile.js"></script>
+<script src="../assets/vendor/bootstrap/js/bootstrap.js"></script>
+<script src="../assets/vendor/nanoscroller/nanoscroller.js"></script>
+<script src="../assets/vendor/bootstrap-datepicker/js/bootstrap-datepicker.js"></script>
+<script src="../assets/vendor/magnific-popup/magnific-popup.js"></script>
+<script src="../assets/vendor/jquery-placeholder/jquery.placeholder.js"></script>
+
+<!-- Theme Base, Components and Settings -->
+<script src="../assets/javascripts/theme.js"></script>
+
+<!-- Theme Custom -->
+<script src="../assets/javascripts/theme.custom.js"></script>
+
+<!-- Theme Initialization Files -->
+<script src="../assets/javascripts/theme.init.js"></script>
+
+
+<!--===============================================================================================-->
+<script src="../assets/vendor/animsition/js/animsition.min.js"></script>
+<!--===============================================================================================-->
+<script src="../assets/vendor/bootstrap/js/popper.js"></script>
+<script src="../assets/vendor/bootstrap/js/bootstrap.min.js"></script>
+<!--===============================================================================================-->
+<script src="../assets/vendor/select2/select2.min.js"></script>
+<!--===============================================================================================-->
+<script src="../assets/vendor/daterangepicker/moment.min.js"></script>
+<script src="../assets/vendor/daterangepicker/daterangepicker.js"></script>
+<!--===============================================================================================-->
+<script src="../assets/vendor/countdowntime/countdowntime.js"></script>
+<!--===============================================================================================-->
+<script src="js/main.js"></script>
+
+</body>
+</html>
+
+<?php
+
+function getInfo(){
+    $servername = "localhost";
+    $username = "decision_making";
+    $password = "";
+
+// Create connection
+//$conn = mysqli_connect($servername, $username, $password);
+    $conn = mysqli_connect($servername, "root",$password, $username,"3306");
+// Check connection
+    if (!$conn) {
+        die("Connection failed: " . mysqli_connect_error());
+    }
+    mysqli_set_charset($conn,"utf8");
+
+
+
+
+    $query = "SELECT * FROM `users` WHERE `type` = 'supervisor'";
+
+    $result = $conn->query($query);
+
+    if ($result->num_rows > 0) {
+        $info = [];
+        while ($row = $result->fetch_assoc()) {
+            array_push($info,
+                ["name" => $row["name"],
+                "email" => $row["email"],
+                "number" => $row["number"],
+                "age" => $row["age"],
+                "gender" => $row["gender"],
+                "level" => $row["level"],
+                "degree" => $row["degree"],
+                "mobile_no" => $row["mobile_no"],
+                "country" => $row["country"]
+                ]);
+        }
+        return $info;
+        //echo $sl_number;
+    } else {
+        header('Location: ../sign_in.php');
+    }
+}
+
+
+?>
